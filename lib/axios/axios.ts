a57314f0847ef0a2828de85dcefa67b4dec3e7a4
L <lillustrer012@gmail.com>
2025-09-27 23:29:15 +0700

    feat: add axios global method

---
+++ lib/axios/axios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { NEXT_PUBLIC_BACKEND_URL } from "../utils/constants";
+
+const apiClient = axios.create({
+	baseURL: NEXT_PUBLIC_BACKEND_URL,
+	timeout: 10000,
+	headers: {
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+	// withCredentials: true,
+});
+
+apiClient.interceptors.response.use((config) => {
+	const accessToken = localStorage.getItem("accessToken");
+
+	if (accessToken) {
+		config.headers.Authorization = `Bearer ${accessToken}`;
+	}
+
+	return config;
+});
+
+export default apiClient;
