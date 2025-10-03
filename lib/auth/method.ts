f0e3016c08602c5aadd296465e58d8bf883b012f
L <lillustrer012@gmail.com>
2025-10-03 15:45:30 +0700

    feat: add signout route

--- lib/auth/method.ts
+++ lib/auth/method.ts
@@ -1,14 +1,75 @@
-import { handleApiError, type ErrorResponse } from "../api/errors";
-import { POST } from "../axios/method";
-import { SignInUser } from "../types/signIn";
+import { ErrorResponse, handleApiError } from "../api/errors";
+import { POST } from "../axios";
+import {
+	SignInAdmin,
+	SignInUser,
+	SignUpAdmin,
+	SignUpUser,
+} from "../types/auth";
+import { ROUTES } from "./routes";
+import Cookies from "js-cookie";
 
 export const signInUser = async (
 	data: SignInUser,
 ): Promise<any | ErrorResponse> => {
 	try {
-		const res = await POST("/auth/login", data);
+		const res = await POST(ROUTES.STUDENT.SIGNIN, data);
 
 		return res;
+	} catch (error) {
+		const errorResponse = handleApiError(error);
+
+		return errorResponse;
+	}
+};
+
+export const signUpUser = async (
+	data: SignUpUser,
+): Promise<any | ErrorResponse> => {
+	try {
+		const res = await POST(ROUTES.STUDENT.SIGNUP, data);
+
+		return res;
+	} catch (error) {
+		const errorResponse = handleApiError(error);
+
+		return errorResponse;
+	}
+};
+
+export const signUpAdmin = async (
+	data: SignUpAdmin,
+): Promise<any | ErrorResponse> => {
+	try {
+		const res = await POST(ROUTES.ADMIN.SIGNUP, data);
+
+		return res;
+	} catch (error) {
+		const errorResponse = handleApiError(error);
+
+		return errorResponse;
+	}
+};
+
+export const signInAdmin = async (
+	data: SignInAdmin,
+): Promise<any | ErrorResponse> => {
+	try {
+		const res = await POST(ROUTES.ADMIN.SIGNIN, data);
+
+		return res;
+	} catch (error) {
+		const errorResponse = handleApiError(error);
+
+		return errorResponse;
+	}
+};
+
+export const signOut = async () => {
+	try {
+		Cookies.remove("accessToken");
+
+		// return res;
 	} catch (error) {
 		const errorResponse = handleApiError(error);
 
