b7a36c9a1c5cf5bfd60b65477dbb1203136b2770
L <lillustrer012@gmail.com>
2025-10-03 22:47:22 +0700

    add lecturer api routes

---
+++ lib/auth/api-routes.ts
@@ -0,0 +1,14 @@
+export const APIROUTES = {
+	ADMIN: {
+		SIGNUP: "/auth/admin/signup",
+		SIGNIN: "/auth/admin/signin",
+	},
+	STUDENT: {
+		SIGNUP: "/auth/student/signup",
+		SIGNIN: "/auth/student/signin",
+	},
+	LECTURER: {
+		SIGNUP: "/auth/lecturer/signup",
+		SIGNIN: "/auth/lecturer/signin",
+	},
+};
