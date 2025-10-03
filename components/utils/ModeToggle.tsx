f7bff7f114de6e0317dfa84b26dcd5a970b683c8
L <lillustrer012@gmail.com>
2025-10-03 19:07:43 +0700

    feat: add light/dark theme toggle

---
+++ components/utils/ModeToggle.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
+
+export function ModeToggle() {
+	const { theme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) {
+		return null;
+	}
+
+	return (
+		<div>
+			{theme === "light" ? (
+				<Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
+					<Moon className="h-[1.2rem] w-[1.2rem]" />
+				</Button>
+			) : (
+				<Button variant="outline" size="icon" onClick={() => setTheme("light")}>
+					<Sun className="h-[1.2rem] w-[1.2rem]" />
+				</Button>
+			)}
+		</div>
+	);
+}
