09b51b483af88537c22683ea11318dc09bde5bd7
L <lillustrer012@gmail.com>
2025-10-07 02:09:51 +0700

    feat: add use-auth hook

---
+++ components/ui/hooks/use-auth.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+export const useAuth = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+		const token = Cookies.get("accessToken");
+
+		setIsAuthenticated(!!token);
+	}, []);
+
+	return isAuthenticated;
+};
