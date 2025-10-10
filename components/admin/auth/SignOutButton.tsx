f18922ba7e865b73f8e83cf5c6eaf847e000f23f
L <lillustrer012@gmail.com>
2025-10-10 22:02:29 +0700

    feat: add sign out button

---
+++ components/admin/auth/SignOutButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import { toast } from "sonner";
+import { ROUTES } from "@/lib/utils";
+import { useSession } from "@/components/provider/SessionProvider";
+import { Button } from "../../ui/shadcn";
+
+export const SignOutButton = () => {
+	const router = useRouter();
+	const { logout } = useSession();
+
+	const handleLogout = async () => {
+		try {
+			if (!Cookies.get("accessToken")) {
+				console.log("Access token not found");
+			}
+
+			logout();
+
+			toast.success("Signed out successfully");
+
+			router.push(ROUTES.adminSite.auth.signin);
+		} catch {
+			console.error("Something went wrong");
+
+			toast.error("An unexpected error occurred while logging out.");
+		}
+	};
+
+	return <Button onClick={handleLogout}>Sign out</Button>;
+};
