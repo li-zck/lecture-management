5f489e184ba5686f99f9d3536f842560e35fa565
L <lillustrer012@gmail.com>
2025-10-10 22:00:56 +0700

    update admin/management route layouts

---
+++ app/(cms)/admin/(management)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { ErrorBoundary } from "@/components/ui/error";
+
+export default function ManagementPageLayout({
+	children,
+}: {
+	children: ReactNode;
+}) {
+	return (
+		<ErrorBoundary>
+			{children}
+		</ErrorBoundary>
+	);
+}
