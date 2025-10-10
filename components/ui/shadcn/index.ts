4ac9723fa8da0e5196e30e12dacd688660681556
L <lillustrer012@gmail.com>
2025-10-10 22:04:16 +0700

    update shadcn components

---
+++ components/ui/shadcn/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./form";
+export * from "./input";
+export * from "./label";
+export * from "./sonner";
+export * from "./skeleton";
+export * from "./button-group";
