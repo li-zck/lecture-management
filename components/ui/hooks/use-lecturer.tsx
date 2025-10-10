b28ef7744ac05c1e64188015a8d54d59dd9ef01b
L <lillustrer012@gmail.com>
2025-10-10 22:03:58 +0700

    feat: add useLecturer() hook

---
+++ components/ui/hooks/use-lecturer.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { getAllLecturerAccounts } from "@/lib/admin/api/read/method";
+import { ReadAllLecturerAccountResponse } from "@/lib/types/dto/api/admin/response/read/read-account.dto";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { toast } from "sonner";
+
+export const useLecturers = () => {
+	const [lecturers, setLecturers] = useState<ReadAllLecturerAccountResponse>(
+		[],
+	);
+	const [totalLecturers, setTotalLecturers] = useState(0);
+	const [loading, setLoading] = useState(true);
+
+	const fetchLecturers = useCallback(async () => {
+		try {
+			const res = await getAllLecturerAccounts();
+			const lecturers = Array.isArray(res.data) ? res.data : [];
+
+			setLecturers(lecturers);
+			setTotalLecturers(lecturers.length);
+		} catch (error) {
+			toast.error("Failed to get lecturer data");
+		} finally {
+			setLoading(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		fetchLecturers();
+	}, [fetchLecturers]);
+
+	return useMemo(
+		() => ({ lecturers, totalLecturers, loading, refetch: fetchLecturers }),
+		[lecturers, totalLecturers, loading, fetchLecturers],
+	);
+};
