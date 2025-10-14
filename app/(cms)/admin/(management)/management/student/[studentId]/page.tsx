bede560e2492d51671fc6ede0a8d4f3b1e60eb27
L <lillustrer012@gmail.com>
2025-10-14 09:17:45 +0700

    add student details page

---
+++ app/(cms)/admin/(management)/management/student/[studentId]/page.tsx
@@ -0,0 +1,16 @@
+import { DetailsPage } from "@/components/admin/view";
+
+export default async function StudentDetailsPage({
+	params,
+}: {
+	params: Promise<{ studentId: string }>;
+}) {
+	const { studentId } = await params;
+
+	return (
+		<div>
+			<DetailsPage entityId={studentId} entityType="student" />
+			Student ID: {studentId}
+		</div>
+	);
+}
