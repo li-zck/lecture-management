f1ab1d5df5d1cfc07570119e3ed10febe99ec070
L <lillustrer012@gmail.com>
2025-10-14 09:21:29 +0700

    update dto for student, lecturer, and department

---
+++ lib/types/dto/api/admin/response/read/read.dto.ts
@@ -0,0 +1,43 @@
+export type StudentAccountResponse = {
+	id: string;
+	departmentId: string;
+	username: string;
+	email: string;
+	studentId: string;
+	fullName: string;
+	gender: boolean;
+	birthDate: string;
+	citizenId: string;
+	phone: string;
+	address: string;
+	active: boolean;
+	createdAt: string;
+	updatedAt: string;
+};
+
+export type ReadAllStudentAccountsResponse = StudentAccountResponse[];
+
+export type LecturerAccountResponse = {
+	id: string;
+	lecturerId: string; // this field is missing from backend
+	email: string;
+	username: string;
+	fullName: string;
+	active: boolean;
+	createdAt: string;
+	updatedAt: string;
+};
+
+export type ReadAllLecturerAccountResponse = LecturerAccountResponse[];
+
+export type DepartmentResponse = {
+	id: string;
+	departmentId: string; // this field is missing from backend
+	name: string;
+	description?: string;
+	headId?: string;
+	createdAt: string;
+	updatedAt: string;
+};
+
+export type ReadAllDepartmentResponse = DepartmentResponse[];
