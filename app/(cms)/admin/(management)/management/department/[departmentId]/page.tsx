c4f5098361ded2ec9db1e7c70511f86e3ad7316e
L <lillustrer012@gmail.com>
2025-10-14 09:17:31 +0700

    add department details page

---
+++ app/(cms)/admin/(management)/management/department/[departmentId]/page.tsx
@@ -0,0 +1,16 @@
+import { DetailsPage } from "@/components/admin/view";
+
+export default async function StudentDetailsPage({
+	params,
+}: {
+	params: Promise<{ departmentId: string }>;
+}) {
+	const { departmentId } = await params;
+
+	return (
+		<div>
+			<DetailsPage entityId={departmentId} entityType="department" />
+			Department ID: {departmentId}
+		</div>
+	);
+}
