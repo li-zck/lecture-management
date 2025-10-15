ddeba884a6b8d422edfaa7e979bda67b4081b44c
L <lillustrer012@gmail.com>
2025-10-16 02:44:26 +0700

    update next config to handle postgres warning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// suppress postgresql warnings
+const originalConsoleWarn = console.warn;
+
+console.warn = (...args) => {
+	const message = args.join(" ");
+
+	if (message.includes("Package pg can't be external")) {
+		return;
+	}
+
+	originalConsoleWarn.apply(console, args);
 };
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
