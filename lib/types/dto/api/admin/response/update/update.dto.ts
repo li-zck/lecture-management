670f0b9944bd779ffc61634d65b15d500d0e54a3
L <lillustrer012@gmail.com>
2025-10-16 02:43:29 +0700

    add update dto responses

---
+++ lib/types/dto/api/admin/response/update/update.dto.ts
@@ -0,0 +1,11 @@
+import {
+	CreateDepartmentResponse,
+	CreateLecturerAccountResponse,
+	CreateStudentAccountResponse,
+} from "../create";
+
+export type UpdateStudentAccountResponse = CreateStudentAccountResponse;
+
+export type UpdateLecturerAccountResponse = CreateLecturerAccountResponse;
+
+export type UpdateDepartmentResponse = CreateDepartmentResponse;
