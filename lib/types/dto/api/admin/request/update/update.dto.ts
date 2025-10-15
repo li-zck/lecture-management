b66ea30082cb9c702818e4aca6ff616139994886
L <lillustrer012@gmail.com>
2025-10-16 02:42:57 +0700

    add update dtos

---
+++ lib/types/dto/api/admin/request/update/update.dto.ts
@@ -0,0 +1,30 @@
+export type UpdateStudentAccountRequest = {
+	studentId?: string;
+	departmentId?: string;
+	email?: string;
+	username?: string;
+	newPassword?: string;
+	confirmPassword?: string;
+	fullName?: string;
+	gender?: boolean;
+	birthDate?: string;
+	citizenId?: string;
+	phone?: string;
+	address?: string;
+	active?: boolean;
+};
+
+export type UpdateLecturerAccountRequest = {
+	email?: string;
+	username?: string;
+	newPassword?: string;
+	confirmPassword?: string;
+	fullName?: string;
+	isActive?: boolean;
+};
+
+export type UpdateDepartmentRequest = {
+	name?: string;
+	description?: string;
+	headId?: string;
+};
