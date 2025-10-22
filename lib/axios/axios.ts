cdb9cbbf516324b202eaf9a2997bdade972494ac
L <lillustrer012@gmail.com>
2025-10-22 22:31:54 +0700

    adjust imports

--- lib/axios/axios.ts
+++ lib/axios/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-import { BACKEND_URL, ROUTES } from "../utils";
+import { BACKEND_URL } from "../utils";
 
 const apiClient = axios.create({
 	baseURL: BACKEND_URL,
