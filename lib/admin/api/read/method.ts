e2a58384c98534c02016f6e1c53b9dc9e9664365
L <lillustrer012@gmail.com>
2025-11-03 22:00:58 +0700

    update read methods to include admin details read

--- lib/admin/api/read/method.ts
+++ lib/admin/api/read/method.ts
@@ -1,59 +1,69 @@
 import { getErrorMessage } from "@/lib/api";
-import { ApiResponse, GET } from "@/lib/axios";
-import {
-	ReadAllLecturerAccountResponse,
-	ReadAllStudentAccountsResponse,
-	ReadAllDepartmentResponse,
-	StudentAccountResponse,
-	LecturerAccountResponse,
-	DepartmentResponse,
+import { type ApiResponse, GET } from "@/lib/axios";
+import type {
+  DepartmentResponse,
+  LecturerAccountResponse,
+  ReadAdminAccountResponse,
+  ReadAllAdminAccountsResponse,
+  ReadAllDepartmentResponse,
+  ReadAllLecturerAccountResponse,
+  ReadAllStudentAccountsResponse,
+  StudentAccountResponse,
 } from "@/lib/types/dto/api/admin/response/read/read.dto";
 import { APIROUTES } from "@/lib/utils";
 
 const get = async <TResponse>(
-	route: string,
+  route: string,
 ): Promise<ApiResponse<TResponse>> => {
-	try {
-		const res = await GET<TResponse>(route);
+  try {
+    const res = await GET<TResponse>(route);
 
-		return res;
-	} catch (error: any) {
-		const status = error.response?.status || 500;
-		const message = getErrorMessage(status);
+    return res;
+  } catch (error: any) {
+    const status = error.response?.status || 500;
+    const message = getErrorMessage(status);
 
-		throw { status, message };
-	}
+    throw { status, message };
+  }
 };
 
 /*
  * Student methods
  * */
 export const getAllStudentAccounts = () =>
-	get<ReadAllStudentAccountsResponse>(APIROUTES.admin.view.student.all);
+  get<ReadAllStudentAccountsResponse>(APIROUTES.admin.view.student.all);
 
 export const getStudentById = (studentId: string) =>
-	get<StudentAccountResponse>(
-		APIROUTES.admin.view.student.each.replace(":id", studentId),
-	);
+  get<StudentAccountResponse>(
+    APIROUTES.admin.view.student.each.replace(":id", studentId),
+  );
 
 /*
  * Lecturer methods
  * */
 export const getAllLecturerAccounts = () =>
-	get<ReadAllLecturerAccountResponse>(APIROUTES.admin.view.lecturer.all);
+  get<ReadAllLecturerAccountResponse>(APIROUTES.admin.view.lecturer.all);
 
 export const getLecturerById = (lecturerId: string) =>
-	get<LecturerAccountResponse>(
-		APIROUTES.admin.view.lecturer.each.replace(":id", lecturerId),
-	);
+  get<LecturerAccountResponse>(
+    APIROUTES.admin.view.lecturer.each.replace(":id", lecturerId),
+  );
 
 /*
  * Department methods
  * */
 export const getAllDepartments = () =>
-	get<ReadAllDepartmentResponse>(APIROUTES.admin.view.department.all);
+  get<ReadAllDepartmentResponse>(APIROUTES.admin.view.department.all);
 
 export const getDepartmentById = (departmentId: string) =>
-	get<DepartmentResponse>(
-		APIROUTES.admin.view.department.each.replace(":id", departmentId),
-	);
+  get<DepartmentResponse>(
+    APIROUTES.admin.view.department.each.replace(":id", departmentId),
+  );
+
+export const getAllAdmins = () =>
+  get<ReadAllAdminAccountsResponse>(APIROUTES.admin.view.admin.all);
+
+export const getAdminById = (adminId: string) =>
+  get<ReadAdminAccountResponse>(
+    APIROUTES.admin.view.admin.each.replace(":id", adminId),
+  );
