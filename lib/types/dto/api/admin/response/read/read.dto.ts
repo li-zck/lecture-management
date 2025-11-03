d9b3a7a399f1af393f7f1cea6057e4645669af0d
L <lillustrer012@gmail.com>
2025-11-03 22:01:34 +0700

    update admin account read dtos

--- lib/types/dto/api/admin/response/read/read.dto.ts
+++ lib/types/dto/api/admin/response/read/read.dto.ts
@@ -1,43 +1,56 @@
+import { Edu_AU_VIC_WA_NT_Pre } from "next/font/google";
+
 export type StudentAccountResponse = {
-	id: string;
-	departmentId: string;
-	username: string;
-	email: string;
-	studentId: string;
-	fullName: string;
-	gender: boolean;
-	birthDate: string;
-	citizenId: string;
-	phone: string;
-	address: string;
-	active: boolean;
-	createdAt: string;
-	updatedAt: string;
+  id: string;
+  departmentId: string;
+  username: string;
+  email: string;
+  studentId: string;
+  fullName: string;
+  gender: boolean;
+  birthDate: string;
+  citizenId: string;
+  phone: string;
+  address: string;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type ReadAllStudentAccountsResponse = StudentAccountResponse[];
 
 export type LecturerAccountResponse = {
-	id: string;
-	lecturerId: string; // this field is missing from backend
-	email: string;
-	username: string;
-	fullName: string;
-	active: boolean;
-	createdAt: string;
-	updatedAt: string;
+  id: string;
+  lecturerId: string; // this field is missing from backend
+  email: string;
+  username: string;
+  fullName: string;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type ReadAllLecturerAccountResponse = LecturerAccountResponse[];
 
 export type DepartmentResponse = {
-	id: string;
-	departmentId: string; // this field is missing from backend
-	name: string;
-	description?: string;
-	headId?: string;
-	createdAt: string;
-	updatedAt: string;
+  id: string;
+  departmentId: string; // this field is missing from backend
+  name: string;
+  description?: string;
+  headId?: string;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type ReadAllDepartmentResponse = DepartmentResponse[];
+
+export type ReadAdminAccountResponse = {
+  id: string;
+  username: string;
+  password: string;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type ReadAllAdminAccountsResponse = ReadAdminAccountResponse[];
