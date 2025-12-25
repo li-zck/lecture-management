be3d442694f1e174cf4b813347c7f7c3d5e05bd5
L <lillustrer012@gmail.com>
2025-12-25 23:46:33 +0700

    update main sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,93 +1,78 @@
 "use client";
 
 import Cookies from "js-cookie";
-import Link from "next/link";
 import { Suspense, useState } from "react";
 import { SignInForm } from "@/components/auth/SignInForm";
 import RoleSelector from "@/components/ui/RoleSelector";
-import { Button } from "@/components/ui/shadcn/button";
 import { signInLecturer, signInStudent } from "@/lib/auth";
 import { signInLecturerSchema, signInStudentSchema } from "@/lib/zod";
+import { Wordmark } from "@/components/ui";
 
 const SignInPage = () => {
-	const [role, setRole] = useState<"student" | "lecturer">("student");
+  const [role, setRole] = useState<"student" | "lecturer">("student");
 
-	const schema =
-		role === "student" ? signInStudentSchema : signInLecturerSchema;
+  const schema =
+    role === "student" ? signInStudentSchema : signInLecturerSchema;
 
-	const fields = [
-		{
-			name: role === "student" ? "studentId" : "lecturerId",
-			label: role === "student" ? "Student ID" : "Lecturer ID",
-			placeholder: "johndoe",
-		},
-		{
-			name: "password",
-			label: "Password",
-			placeholder: "********",
-			type: "password",
-		},
-	];
+  const fields = [
+    {
+      name: role === "student" ? "studentId" : "lecturerId",
+      label: role === "student" ? "Student ID" : "Lecturer ID",
+      placeholder: "johndoe",
+    },
+    {
+      name: "password",
+      label: "Password",
+      placeholder: "********",
+      type: "password",
+    },
+  ];
 
-	const defaultValues =
-		role === "student"
-			? { studentId: "", password: "" }
-			: { lecturerId: "", password: "" };
+  const defaultValues =
+    role === "student"
+      ? { studentId: "", password: "" }
+      : { lecturerId: "", password: "" };
 
-	const handleSubmit = async (values: any) => {
-		const res =
-			role === "student"
-				? await signInStudent(values)
-				: await signInLecturer(values);
+  const onSubmit = async (values: any) => {
+    const res =
+      role === "student"
+        ? await signInStudent(values)
+        : await signInLecturer(values);
 
-		Cookies.set("accessToken", res.data.accessToken, {
-			path: "/",
-			expires: 365,
-			secure: process.env.NODE_ENV === "production",
-			sameSite: "strict",
-		});
-	};
+    Cookies.set("accessToken", res.data.accessToken, {
+      path: "/",
+      expires: 365,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+  };
 
-	return (
-		<div className="min-h-screen flex items-center justify-center">
-			<div className="max-w-md w-full space-y-8">
-				<div className="text-center">
-					<Link href="/">
-						<Button variant="outline">Logo</Button>
-					</Link>
-				</div>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md w-full space-y-8">
+        <div className="flex justify-center">
+          <Wordmark />
+        </div>
 
-				<div>
-					<h2 className="mt-6 mb-3 text-center text-3xl font-extrabold">
-						Welcome Back
-					</h2>
+        <div>
+          <h2 className="mt-6 mb-3 text-center text-3xl font-extrabold">
+            Welcome Back
+          </h2>
 
-					<Suspense fallback={<div>Loading...</div>}>
-						<RoleSelector value={role} onChange={setRole} />
-					</Suspense>
+          <Suspense fallback={<div>Loading...</div>}>
+            <RoleSelector value={role} onChange={setRole} />
+          </Suspense>
 
-					<SignInForm
-						schema={schema}
-						fields={fields}
-						onSubmitAction={handleSubmit}
-						defaultValues={defaultValues}
-					/>
-
-					<div className="pt-4">
-						<p className="text-center">
-							Or{" "}
-							<Link
-								href="/sign-up"
-								className="font-medium text-indigo-600 hover:text-indigo-500"
-							>
-								create a new account
-							</Link>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <SignInForm
+            schema={schema}
+            fields={fields}
+            onSubmitAction={onSubmit}
+            defaultValues={defaultValues}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
