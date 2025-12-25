46e44f17b60e6fc91526ffe07a41a014ca38dadd
L <lillustrer012@gmail.com>
2025-12-25 23:50:07 +0700

    feat: add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { Loading } from "@/components/ui";
+
+function LoadingPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Loading />
+    </div>
+  );
+}
+
+export default LoadingPage;
