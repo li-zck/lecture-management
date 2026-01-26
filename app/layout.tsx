8d03be11c44cae7021fa397c5397136a9c25a0b9
L <lillustrer012@gmail.com>
2026-01-26 18:20:54 +0700

    update root styling and configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,25 +4,25 @@ import "./globals.css";
 import RootProviders from "./providers";
 
 const robotoMono = Roboto({
-	variable: "--font-roboto-mono",
-	subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "Lecture Management Site",
-	description: "Build with Nextjs",
+  title: "Lecture Management Site",
+  description: "Build with Nextjs",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={`${robotoMono.variable} antialiased`}>
-				<RootProviders>{children}</RootProviders>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${robotoMono.variable} antialiased`}>
+        <RootProviders>{children}</RootProviders>
+      </body>
+    </html>
+  );
 }
